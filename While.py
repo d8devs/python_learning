880c65df6f4f4c3231f559db7a98b1327ceb5bbc
Koray Zorluoglu <koray@d8devs.com>
2018-06-06 12:53:30 +0200

    Step 5: While

---
+++ While.py
@@ -0,0 +1,13 @@
+#################################
+# Playing with String
+print('#'*33)
+#################################
+
+'''
+    While
+'''
+
+counter = 0
+while counter < 10:
+    print(counter)
+    counter = counter + 1
