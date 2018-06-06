9802342ab49effe5ec88b1771ff0ed8c1bd999ed
Koray Zorluoglu <koray@d8devs.com>
2018-06-06 11:34:00 +0200

    Step 1: Strings and Integers

---
+++ Strings_Numbers.py
@@ -0,0 +1,35 @@
+import random
+
+
+#################################
+#Playing with String
+print('+'*33)
+#################################
+
+'''
+    int to str
+'''
+age = random.randint(14,90)
+print("Selam !")
+print("Age : " + str(age))
+
+
+#################################
+#Playing with String
+print('*'*33)
+#################################
+
+''' 
+    Write String with Format
+                 With Multiple Placeholder
+'''
+gender = "Male"
+
+print("Age : {} ".format(age))
+print("Age : {}, Gender : {} ".format(age, gender))
+
+
+#################################
+#Playing with String
+print('#'*33)
+#################################
