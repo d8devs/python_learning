596e3f1f6b422cfcdf5adc3ce2f1e856c18d2b46
Koray Zorluoglu <koray@d8devs.com>
2018-06-06 13:00:50 +0200

    Step 6: For

---
+++ For.py
@@ -0,0 +1,52 @@
+#################################
+# Playing with String
+print('+'*33)
+#################################
+
+'''
+    For
+'''
+
+for i in range(0, 10):
+    print(i)
+
+#################################
+# Playing with String
+print('+'*33)
+#################################
+
+''' 
+    For with List
+'''
+students = ["Ahmet", "Hasan", "Seyma", "Sare"]
+
+for student in students:
+    print(student)
+
+#################################
+# Playing with String
+print('+' * 33)
+#################################
+
+'''
+    For with continue
+'''
+
+for number in range(0, 10):
+    if number == 2:
+        continue
+    print(number)
+
+#################################
+# Playing with String
+print('+' * 33)
+#################################
+
+'''
+    For with break
+'''
+
+for number in range(0, 10):
+    if number == 4:
+        break
+    print(number)
