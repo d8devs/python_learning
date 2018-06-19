f5581e3f65cb90c703408fb387f129b3dc745578
Koray Zorluoglu <koray@d8devs.com>
2018-06-19 16:52:13 +0200

    Step 11: Graphic Creating with matplotlib

---
+++ Graphics.py
@@ -0,0 +1,16 @@
+#################################
+#Playing with String
+print('*'*33)
+#################################
+'''
+    Graphic Create a Simple Graph with Matplotlib
+'''
+
+import matplotlib.pyplot as plt
+
+
+xs = [1,2,3,7]
+ys = [4,7,4,1]
+
+plt.plot(xs, ys)
+plt.show()
