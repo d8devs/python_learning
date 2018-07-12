808db192811843db70f5525d52684557756b4afc
Koray Zorluoglu <koray@d8devs.com>
2018-07-12 11:50:05 +0200

    Step 18: OOP - Class Variable Update over Method

--- oop/class.py
+++ oop/class.py
@@ -42,3 +42,31 @@ class Student():
 koray = Student("d8", "devs")
 # run Class method
 koray.getFullname()
+
+
+
+'''
+    Create Class and Methods for Example
+'''
+
+class Student():
+    def __init__(self, name, lastname):
+        self.name = name
+        self.lastname = lastname
+        # set standart variable with value
+        self.term = 1
+
+    def getFullname(self):
+        # add term variable
+        print(self.name + "" + self.lastname + "(Semester " + str(self.term) + ")")
+
+    def increase_term(self):
+        self.term = self.term + 1
+
+# Create a Student
+koray = Student("d8", "devs")
+# run Class method
+koray.getFullname()
+# update this student semester with good solution/oop best practice way
+koray.increase_term()
+koray.getFullname()
