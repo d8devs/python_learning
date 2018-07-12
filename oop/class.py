24d2ae9f92aeb4b11de197470af7eeca101af0c0
Koray Zorluoglu <koray@d8devs.com>
2018-07-12 11:37:46 +0200

    Step 17: OOP - Class

---
+++ oop/class.py
@@ -0,0 +1,26 @@
+#################################
+#Playing with String
+print('*'*33)
+#################################
+
+'''
+    Create Class
+'''
+class Teacher():
+    pass
+
+
+# Create a Teacher using Teacher Class
+erbakan = Teacher()
+erbakan.name = "Necmettin"
+erbakan.lastname = "Erbakan"
+
+# Create a another Teacher with same Teacher Class
+karamollaoglu = Teacher()
+karamollaoglu.name = "Temel"
+karamollaoglu.lastname = "Karamollaoglu"
+
+print(erbakan.name)
+print(erbakan.lastname)
+print(karamollaoglu.name)
+print(karamollaoglu.lastname)
