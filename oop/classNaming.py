82f3e12accc75d1d9fd22a8ced48f9279a935b30
Koray Zorluoglu <koray@d8devs.com>
2018-07-13 13:06:18 +0200

    Step 27: OOP - Class/Variable/Method Naming

---
+++ oop/classNaming.py
@@ -0,0 +1,14 @@
+
+# PascalCase (ForExampleVariable) => Best Practice for Class naming
+# camelCase (forExampleVariable) => Best Practice for Method naming
+# sneak_case (for_example_variable)
+
+
+class for_example_variable():
+    pass
+
+class forExampleVariable():
+    pass
+
+class ForExampleVariable():
+    pass
