5e49cdf5a40e521513cee23e96c7665dcde8293f
Koray Zorluoglu <koray@d8devs.com>
2018-11-07 16:00:34 +0100

    Website Crawler with CSV Operations

---
+++ Play/WebsiteCrawlerToCSV.py
@@ -0,0 +1,52 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+import csv
+
+class Article():
+    def __init__(self, title, date, link):
+        self.title = title
+        self.date = date
+        self.link = link
+
+
+class WebpageCrawler():
+    def getArticles(self):
+        url = "https://d8devs.com"
+        articles = []
+
+        while url != "":
+            time.sleep(1)
+            r = requests.get(url)
+            doc = BeautifulSoup(r.text, "html.parser")
+
+            for article in doc.select("article"):
+                link = article.select_one(".wrap-entry-meta span a")['href']
+                date = article.select_one(".wrap-entry-meta time").text
+                title = article.select_one(".entry-title a").text
+
+                newArticle = Article(title, date, link)
+                articles.append(newArticle)
+
+                nextPage = doc.select_one('.nav-links .next')
+
+                if nextPage:
+                    url = nextPage['href']
+                else:
+                    url = ""
+        return articles
+
+
+
+webpageCrawler = WebpageCrawler()
+articles = webpageCrawler.getArticles()
+
+with open('websiteArticles.csv', 'w', newline='', encoding='utf-8') as csvfile:
+    articleWriter = csv.writer(csvfile, delimiter = ';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+    for article in articles:
+        row = "Article : {} {} - {}".format(article.title, article.date, article.link)
+        articleWriter.writerow([article.title, article.date, article.link])
+
+
+
