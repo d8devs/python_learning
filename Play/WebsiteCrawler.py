f304e61d3538f95daf81b5dcae740f61f9c720c8
Koray Zorluoglu <koray@d8devs.com>
2018-11-07 15:18:13 +0100

    Website Crawler with All Pages

--- Play/WebsiteCrawler.py
+++ Play/WebsiteCrawler.py
@@ -1,20 +1,47 @@
 import requests
 from bs4 import BeautifulSoup
+import time
 
-r = requests.get("http://d8devs.com")
+class Article():
+    def __init__(self, title, date, link):
+        self.title = title
+        self.date = date
+        self.link = link
 
-doc = BeautifulSoup(r.text, "html.parser")
+
+class WebpageCrawler():
+    def getArticles(self):
+        url = "https://d8devs.com"
+        articles = []
+
+        while url != "":
+            time.sleep(1)
+            r = requests.get(url)
+            doc = BeautifulSoup(r.text, "html.parser")
+
+            for article in doc.select("article"):
+                link = article.select_one(".wrap-entry-meta span a")['href']
+                date = article.select_one(".wrap-entry-meta time").text
+                title = article.select_one(".entry-title a").text
+
+                newArticle = Article(title, date, link)
+                articles.append(newArticle)
+
+                nextPage = doc.select_one('.nav-links .next')
+
+                if nextPage:
+                    url = nextPage['href']
+                else:
+                    url = ""
+        return articles
 
 
-for paragpText in doc.find_all("p"):
-    print(paragpText.attrs)
-    print(paragpText.text)
 
-for article in doc.select(".post"):
-    title = article.select_one(".entry-title").text.strip('\n')
-    date = article.select_one(".entry-date").text.strip('\n')
+webpageCrawler = WebpageCrawler()
+articles = webpageCrawler.getArticles()
 
-    text = "Title:{} / Date: {}".format(title, date)
-    print(text)
+for article in articles:
+    row = "Article : {} {} - {}".format(article.title, article.date, article.link)
+    print(row)
 
 
