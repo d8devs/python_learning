1de569e3c592a017f86f89e29922faa8d7575546
hgod8 <koray@d8devs.com>
2018-11-17 21:29:47 +0100

    String more functionaly

--- Strings_Numbers.py
+++ Strings_Numbers.py
@@ -1,45 +1,51 @@
 import random
 
-#################################
-# Playing with String
-print('+'*33)
-#################################
-
-'''
-    int to str
-'''
+# int to str
 age = random.randint(14,90)
 print("Selam !")
 print("Age : " + str(age))
 
-
-#################################
-# Playing with String
-print('*'*33)
-#################################
-
-''' 
-    Write String with Format
-                 With Multiple Placeholder
-'''
+# Write String with Format With Multiple Placeholder
 gender = "Male"
-
 print("Age : {} ".format(age))
 print("Age : {}, Gender : {} ".format(age, gender))
 
-# Playing with String
-print('#'*33)
-#################################
-
-'''
-    Find Word Count from Text
-'''
+# Find Word Count from Text
 text = "Lorem Ipsum damit lamor"
 count = len(text.split(" "))
 print(count)
 
-#################################
-# Playing with String
-print('#'*33)
-#################################
-#################################
+# Upper
+print(text.upper())
+
+# Lower
+print(text.lower())
+
+# If string start with "example characters.."
+if text.startswith("L"):
+    print("Text start with >> L <<")
+
+# If string end with "example characters.."
+if text.endswith("r"):
+    print("Text end with >> r <<")
+
+# Remove Characters or Whitespaces
+word = "         Hello ..  "
+print(word.strip(" ."))
+
+# Remove Characters or Whitespaces at the Left Side
+word = "         Hello ..  "
+print(word.lstrip(" "))
+
+
+# Remove Characters or Whitespaces at the Right Side
+word = "         Hello ..  "
+print(word.rstrip(" "))
+
+# Find Character in the Sentence
+print(word.find('e'))
+
+# Replace Character
+print(word.replace("e", "a"))
+
+
